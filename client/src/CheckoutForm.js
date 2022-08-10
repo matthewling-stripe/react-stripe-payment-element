0908d83759e91d7d7e4ae08cd1b0284d06fbbff2
Matthew Ling <matthewling@stripe.com>
2022-08-10 16:47:19 +0100

    Run prettier and change the button working text

--- client/src/CheckoutForm.js
+++ client/src/CheckoutForm.js
@@ -1,8 +1,6 @@
-import {
-  PaymentElement
-} from '@stripe/react-stripe-js'
-import {useState} from 'react'
-import {useStripe, useElements} from '@stripe/react-stripe-js';
+import { PaymentElement } from "@stripe/react-stripe-js";
+import { useState } from "react";
+import { useStripe, useElements } from "@stripe/react-stripe-js";
 
 export default function CheckoutForm() {
   const stripe = useStripe();
@@ -29,11 +27,6 @@ export default function CheckoutForm() {
       },
     });
 
-    // This point will only be reached if there is an immediate error when
-    // confirming the payment. Otherwise, your customer will be redirected to
-    // your `return_url`. For some payment methods like iDEAL, your customer will
-    // be redirected to an intermediate site first to authorize the payment, then
-    // redirected to the `return_url`.
     if (error.type === "card_error" || error.type === "validation_error") {
       setMessage(error.message);
     } else {
@@ -41,18 +34,16 @@ export default function CheckoutForm() {
     }
 
     setIsLoading(false);
-  }
+  };
 
   return (
     <form id="payment-form" onSubmit={handleSubmit}>
       <PaymentElement id="payment-element" />
       <button disabled={isLoading || !stripe || !elements} id="submit">
-        <span id="button-text">
-          {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
-        </span>
+        <span id="button-text">{isLoading ? "Working ... " : "Pay now"}</span>
       </button>
       {/* Show any error or success messages */}
       {message && <div id="payment-message">{message}</div>}
     </form>
-  )
+  );
 }
