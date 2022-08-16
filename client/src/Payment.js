848fb4183218fb2734691418312de94ae732cc94
Matthew Ling <matthewling@stripe.com>
2022-08-16 15:39:24 +0100

    Move loadStripe into the Payment compoent

--- client/src/Payment.js
+++ client/src/Payment.js
@@ -2,10 +2,18 @@ import { useEffect, useState } from "react";
 
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm";
+import { loadStripe } from "@stripe/stripe-js";
 
-function Payment(props) {
-  const { stripePromise } = props;
+function Payment() {
+  const [stripePromise, setStripePromise] = useState(null);
   const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    fetch("/config").then(async (r) => {
+      const { publishableKey } = await r.json();
+      setStripePromise(loadStripe(publishableKey));
+    });
+  }, []);
 
   useEffect(() => {
     fetch("/create-payment-intent", {
